0c69267ee51af86123061f5764549fc1702de9cf
Beau Marwaha <bcm2463@ad.rit.edu>
2016-11-11 21:12:50 -0500

    Added game board rotation
    Added a script that rotates the board with the right and left arrow keys

--- Animal Stackers/Assets/Scripts/BoardManager.cs
+++ Animal Stackers/Assets/Scripts/BoardManager.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Author: Beau Marwaha
+/// Board manager.
+/// </summary>
 public class BoardManager : MonoBehaviour {
+
+	//attributes
+	public float rotation;
+
+	private GameObject gameBoard;
 
 	// Use this for initialization
 	void Start () {
-	
+		//initialize attributes
+		rotation = 0f; 
+		gameBoard = GameObject.FindGameObjectWithTag ("Game Board");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//reset rotation
+		rotation = 0;
+
+		//check for user input for board rotation
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rotation = 1.5f;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			rotation = -1.5f;
+		}
+
+		UpdateRotation ();
 	}
+
+	/// <summary>
+	/// Updates the rotation of the game board.
+	/// </summary>
+	void UpdateRotation(){
+		gameBoard.transform.Rotate(new Vector3(0, rotation, 0));
+	}
+
 }

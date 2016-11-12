a44113c41f4b9adb1e877edc6f675b880e5280d6
Beau Marwaha <bcm2463@ad.rit.edu>
2016-11-11 23:34:14 -0500

    Added Menus
    Added main, rules, and credits menus with buttons to transition between
    them and the game

---
+++ Animal Stackers/Assets/Scripts/InfoMenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Author: Beau Marwaha
+/// Info menu handler.
+/// </summary>
+public class InfoMenuHandler : MonoBehaviour {
+
+	//attributes
+	public Button exitButton;
+	
+	// Use this for initialization
+	void Start () {
+		//inistialize buttons
+		exitButton.GetComponent<Button>().onClick.AddListener(ExitClick);
+	}
+	
+	/// <summary>
+	/// Exits the game on exit button click.
+	/// </summary>
+	void ExitClick(){
+		Application.LoadLevel ("Main Menu"); 
+	}
+}
---
+++ Animal Stackers/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Author: Beau Marwaha
+/// Menu handler.
+/// </summary>
+public class MenuHandler : MonoBehaviour {
+
+	//attributes
+	public Button playButton;
+	public Button rulesButton;
+	public Button creditsButton;
+	public Button exitButton;
+	
+	// Use this for initialization
+	void Start () {
+		//inistialize buttons
+		playButton.GetComponent<Button>().onClick.AddListener(PlayClick);
+		
+		rulesButton.GetComponent<Button>().onClick.AddListener(RulesClick);
+
+		creditsButton.GetComponent<Button>().onClick.AddListener(CreditsClick);
+
+		exitButton.GetComponent<Button>().onClick.AddListener(ExitClick);
+	}
+	
+	/// <summary>
+	/// Starts the game on play button click.
+	/// </summary>
+	void PlayClick(){
+		Application.LoadLevel ("Animal Stackers"); 
+	}
+
+	/// <summary>
+	/// Goes to the Rules menu on button click.
+	/// </summary>
+	void RulesClick(){
+		Application.LoadLevel ("Rules Menu"); 
+	}
+
+	/// <summary>
+	/// Goes to the Credits menu on button click.
+	/// </summary>
+	void CreditsClick(){
+		Application.LoadLevel ("Credits Menu"); 
+	}
+	
+	/// <summary>
+	/// Exits the game on exit button click.
+	/// </summary>
+	void ExitClick(){
+		Application.Quit();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

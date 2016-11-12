1603ed49e995235cccc338db2990c4ecf186bb77
James Troup <jlt9768@ad.rit.edu>
2016-11-11 21:16:04 -0500

    Began Piece Control
    Piece script

---
+++ Animal Stackers/Assets/Scripts/Piece.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour {
+    private Vector3 position;
+    private Vector3 rotation;
+    private Vector3 fallRate;
+	// Use this for initialization
+	void Start () {
+        position = gameObject.transform.position;
+        rotation = gameObject.transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
